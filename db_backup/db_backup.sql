bbd66b22d1d7d338820edaf4d5457bb2fc2bd4ab
JEONGMINGYO <kieesdeolain@gmail.com>
2025-08-07 18:08:12 +0900

    Feat: mysql 백업파일 추가

--- db_backup/db_backup.sql
+++ db_backup/db_backup.sql
@@ -24,13 +24,12 @@ DROP TABLE IF EXISTS `users`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) NOT NULL,
   `nickname` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `uid` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `UK6dotkott2kjsp8vw4d0m25fb7` (`email`),
   UNIQUE KEY `UK2ty1xmrrgtn89xt7kyxx6ta7h` (`nickname`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +38,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'test@example.com','testuser','$2a$10$nqN75E/yrdEKTiOa7zTCluzKjNsHNMRcaj12xK5zzYLMdA4A.Ho/y'),(2,'kieesdeolain@gmail.com','mmmgyo','$2a$10$5DeNcJoh7pEMngI5jcGjnOTkSR0h68XbQ2TdAc4O2uxG2S99yEaqe'),(11,'alsry1811@gmail.com','mingyo','$2a$10$y.lNgLjZNeQSjfaR4LlxJO.X6aU33L5YAs9UwGV8aBAKsKKLUY.Dq'),(12,'hello@gmail.com','world','$2a$10$xh2Jh0kT6GqEx8lMEwLMMucqtHmGgafW5LDzMBZswgehREPNM73XC'),(13,'test@naver.com','wjdalsry','$2a$10$ejEUwCyCoyTy4wu.QX4mFOneD6whYoVLjgHT5JEaQ3gn85C6xYbOC'),(15,'postman@example.com','postman','$2a$10$ZGaffZpqftaG4mHb1CH0/.b8kes9ZzcM8mWXUuBSra0v7uMJcs/Ci'),(16,'testuser@example.com','tester','$2a$10$mS.dv1bvEvUcQwiAddS6x.X0JbX7ert5uAkE37aovfAUORNo6GyjC'),(17,'jeongmingyo@example.com','정민교','$2a$10$Kp8xd2bUGoczGIP57KJsROJx.LSQ0mhEgI4GHDSUmYn6OjKYGyCm.');
+INSERT INTO `users` VALUES (1,'testuser','$2a$10$nqN75E/yrdEKTiOa7zTCluzKjNsHNMRcaj12xK5zzYLMdA4A.Ho/y',''),(2,'mmmgyo','$2a$10$5DeNcJoh7pEMngI5jcGjnOTkSR0h68XbQ2TdAc4O2uxG2S99yEaqe',''),(11,'mingyo','$2a$10$y.lNgLjZNeQSjfaR4LlxJO.X6aU33L5YAs9UwGV8aBAKsKKLUY.Dq',''),(12,'world','$2a$10$xh2Jh0kT6GqEx8lMEwLMMucqtHmGgafW5LDzMBZswgehREPNM73XC',''),(13,'wjdalsry','$2a$10$ejEUwCyCoyTy4wu.QX4mFOneD6whYoVLjgHT5JEaQ3gn85C6xYbOC',''),(15,'postman','$2a$10$ZGaffZpqftaG4mHb1CH0/.b8kes9ZzcM8mWXUuBSra0v7uMJcs/Ci',''),(16,'tester','$2a$10$mS.dv1bvEvUcQwiAddS6x.X0JbX7ert5uAkE37aovfAUORNo6GyjC',''),(17,'정민교','$2a$10$Kp8xd2bUGoczGIP57KJsROJx.LSQ0mhEgI4GHDSUmYn6OjKYGyCm.',''),(18,'jmg','$2a$10$g9xLieOAk5zKe9GMdSEqo.Ux26aJa10GhnrrnJZBtevCiWethur3K','js18111');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-08-07 17:03:55
+-- Dump completed on 2025-08-07 18:06:20
